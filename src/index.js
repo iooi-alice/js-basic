e2fe1cfe769ad1228c6039f3dbfaa84d40637fdd
iooi-alice <kimhee21c@hanmail.net>
2024-09-29 19:06:30 +0900

    refactor: Initialize App component and render it to the DOM

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-const API_URL = 'https://animal-api-two.vercel.app';
+import App from './App.js';
 
-const $content = document.querySelector('div.content');
-let temp = [];
+const $app = document.getElementById('app');
 
-const getData = async () => {
-  let res = await fetch(API_URL);
-
-  try {
-    if (res) {
-      let data = await res.json();
-      data.photos.forEach((item) => {
-        temp += `<img src=${item.url} alt='animail image'>`;
-      });
-
-      $content.innerHTML = temp;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-getData();
+new App($app);
