389cdbf38d118cb072e92b2441caf48ee0b3e090
iooi-alice <kimhee21c@hanmail.net>
2024-09-30 00:54:24 +0900

    refactor: move fetch response inside try block

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,8 @@
 const API_URL = 'https://animal-api-two.vercel.app/';
 
 export const request = async (name) => {
-  let res = await fetch(name ? `${API_URL}${name}` : API_URL);
   try {
+    let res = await fetch(name ? `${API_URL}${name}` : API_URL);
     if (res) {
       let data = await res.json();
       return data.photos;
